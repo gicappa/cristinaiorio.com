8c6f51778d1a264b456601dd62179a811e93f520
Gian Carlo Pace <giancarlo.pace@hyland.com>
2025-06-18 10:22:14 +0200

    Partita IVA added

--- constants.ts
+++ constants.ts
@@ -9,7 +9,7 @@ export const AppConfig = {
   profession: "Psicologa Psicoterapeuta",
   claim: "La mente in continuo movimento",
   contact: {
-    address: "Vicolo Degli Orti 1, 21013 Gallarate (VA), Italia",
+    address: "Vicolo Degli Orti 2, 21013 Gallarate (VA), Italia",
     phone: "+39 347 8035515",
     email: "cristinaiorio@tiscali.it", 
     mapEmbedUrl: "https://maps.google.com/maps?q=Vicolo%20Degli%20Orti%201%2C%2021013%20Gallarate%20VA%2C%20Italy&t=&z=17&ie=UTF8&iwloc=&output=embed"
